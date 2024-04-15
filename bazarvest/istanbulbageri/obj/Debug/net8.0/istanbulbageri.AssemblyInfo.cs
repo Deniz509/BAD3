60d1eaa5edc0aec32f7ecc7b62b093a4b79a8b26
Moneergit <moneerstudy2023@gmail.com>
2024-04-15 14:53:34 +0200

    added comment

--- bazarvest/istanbulbageri/obj/Debug/net8.0/istanbulbageri.AssemblyInfo.cs
+++ bazarvest/istanbulbageri/obj/Debug/net8.0/istanbulbageri.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("istanbulbageri")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af15a385a48dfd1ebed5f4a45388c14e08c13b78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5309832ae5f423c960b1eab5060a4d7ac162bf97")]
 [assembly: System.Reflection.AssemblyProductAttribute("istanbulbageri")]
 [assembly: System.Reflection.AssemblyTitleAttribute("istanbulbageri")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
