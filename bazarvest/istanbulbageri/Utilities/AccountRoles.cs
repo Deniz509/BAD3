a7438eed9bbb7161b0ef26a8e5616d55ae666de2
Jonas Zaki Nahimi <202109834@post.au.dk>
2024-04-29 12:25:24 +0200

    Added accountroles

---
+++ bazarvest/istanbulbageri/Utilities/AccountRoles.cs
@@ -0,0 +1,10 @@
+﻿namespace istanbulbageri.Utilities
+{
+    public static class AccountRoles
+    {
+        public const string Admin = "Admin";
+        public const string Manager = "Manager";
+        public const string Baker = "Baker";
+        public const string Driver = "Driver";
+    }
+}
